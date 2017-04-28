41ff61ec889296c8c1ebb39f9c68c619ca457186
Takahiro Manabe <takahiro.manabe@sony.com>
2017-04-28 19:04:25 +0900

    Fixed Dialer crash issue when finished using dialpad immediately
    Dialer crash occurs when finish dialpad immediately.
    Because dialpad fragment is hidden, although activity has already been destroyed.
    
    Change-Id: I231f3acfd31b877e098b7e036100ff8d8c124985

--- src/com/android/dialer/DialtactsActivity.java
+++ src/com/android/dialer/DialtactsActivity.java
@@ -1265,6 +1265,17 @@ public class DialtactsActivity extends TransactionSafeActivity implements View.O
     }
 
     @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mSlideIn != null) {
+            mSlideIn.setAnimationListener(null);
+        }
+        if (mSlideOut != null) {
+            mSlideOut.setAnimationListener(null);
+        }
+    }
+
+    @Override
     public void onDialpadQueryChanged(String query) {
         mDialpadQuery = query;
         if (mSmartDialSearchFragment != null) {
